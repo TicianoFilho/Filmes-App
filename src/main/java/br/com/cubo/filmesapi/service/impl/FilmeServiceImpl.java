d314d2a647f6a1e0639d70850cd632fbfec0398f
TicianoFilho <ticianofilho@gmail.com>
2023-01-10 18:59:14 -0300

    refactor: add filmeServiceImpl methods implementation

--- src/main/java/br/com/cubo/filmesapi/service/impl/FilmeServiceImpl.java
+++ src/main/java/br/com/cubo/filmesapi/service/impl/FilmeServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.cubo.filmesapi.service.impl;
 
 import br.com.cubo.filmesapi.domain.model.Filme;
+import br.com.cubo.filmesapi.repository.FilmeRepository;
 import br.com.cubo.filmesapi.service.FilmeService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,23 +13,29 @@ import java.util.UUID;
 @Service
 public class FilmeServiceImpl implements FilmeService {
 
+    private final FilmeRepository filmeRepository;
+
+    public FilmeServiceImpl(FilmeRepository filmeRepository) {
+        this.filmeRepository = filmeRepository;
+    }
+
     @Override
     public Filme save(Filme filme) {
-        return null;
+        return filmeRepository.save(filme);
     }
 
     @Override
     public Page<Filme> getAll(Pageable pageable) {
-        return null;
+        return filmeRepository.findAll(pageable);
     }
 
     @Override
     public Optional<Filme> getById(UUID id) {
-        return Optional.empty();
+        return filmeRepository.findById(id);
     }
 
     @Override
     public void delete(Filme filme) {
-
+        filmeRepository.delete(filme);
     }
 }
